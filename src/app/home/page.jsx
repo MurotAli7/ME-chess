6ae94668219ca722ab33281d2e5922488885e343
murodAli7 <murodalieshmamatov@gmai.com>
2025-11-20 10:48:07 +0500

    home +

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -6,9 +6,33 @@ function Home() {
     return (
         <div className="container-fluid p-0">
             <Header />
+            <div className="home">
+                <div className="home-box">
+                    <h1 className="home-title">ME chess cup</h1>
+                    <div className="home-btn-box">
+                        <button className="home-btn">View</button>
+                        <button className="home-btn">Join</button>
+                    </div>
+                </div>
+                <div className="home-box">
+                    <h1 className="home-title">ME chess cup</h1>
+                    <div className="home-btn-box">
+                        <button className="home-btn">View</button>
+                        <button className="home-btn">Join</button>
+                    </div>
+                </div>
+                <div className="home-box">
+                    <h1 className="home-title">ME chess cup</h1>
+                    <div className="home-btn-box">
+                        <button className="home-btn">View</button>
+                        <button className="home-btn">Join</button>
+                    </div>
+                </div>
+            </div>
             <Login />
         </div>
     );
 }
 
 export default Home;
+
--- src/components/header/page.jsx
+++ src/components/header/page.jsx
@@ -5,8 +5,8 @@ function Header() {
         <div className="container">
             <div className="header">
                 <div className="header-logo">
-                    <img src="/M1.jpg" alt="logo" className="logo" />
-                    <h1 className="header-title">ME<br />Chess Cup</h1>
+                    <img src="/ME_CHESS.png" alt="logo" className="logo" />
+                    <h1 className="header-title">ME<br />Chess</h1>
                 </div>
                 <div className="header-link-box">
                     <a href="/" className="header-link">Home</a>
@@ -15,8 +15,8 @@ function Header() {
                     <a href="/admin" className="header-link">Admin</a>
                 </div>
                 <div className="header-box">
-                    <button className="header-btn">Send ME</button>
-                    <button className="header-btn">Contact us</button>
+                    <button className="header-btn">Sign in</button>
+                    <button className="header-btn">Sign up</button>
                     <div className="header-profil-img-box">
                         <img src="/images.png" alt="profile" className="profil-img" />
                     </div>
